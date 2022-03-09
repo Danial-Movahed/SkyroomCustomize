92f198b8eafe9dd8c784d48bf83debab704dbcbb
Seyed Danial Movahed <movaheddanial@gmail.com>
2022-03-09 12:33:35 +0330

    Added nicknames for "Barati", "Kazemi", "Elahian"

--- skyroomCustom/script.js
+++ skyroomCustom/script.js
@@ -92,6 +92,15 @@ setInterval(function(){
     replace(document.getElementsByClassName("col box block users")[0], new RegExp("90515 ارشیا راسفجانی", "g"), "مدرسان شریف");
     replace(document.getElementsByClassName("col box-grow message-box")[0], new RegExp("90527 محمدامین قلی پور", "g"), "CodeNameZig");
     replace(document.getElementsByClassName("col box block users")[0], new RegExp("90527 محمدامین قلی پور", "g"), "CodeNameZig");
+    replace(document.getElementsByClassName("col box-grow message-box")[0], new RegExp("90306 محمدمهدی الهیان", "g"), "داوود خدادوست");
+    replace(document.getElementsByClassName("col box block users")[0], new RegExp("90306 محمدمهدی الهیان", "g"), "داوود خدادوست");
+    replace(document.getElementsByClassName("col box-grow message-box")[0], new RegExp("90308 محمدمتین براتی", "g"), "نور دل پارسایان");
+    replace(document.getElementsByClassName("col box block users")[0], new RegExp("90308 محمدمتین براتی", "g"), "نور دل پارسایان");
+    replace(document.getElementsByClassName("col box-grow message-box")[0], new RegExp("90327 حسین کاظمی", "g"), "دکتر");
+    replace(document.getElementsByClassName("col box block users")[0], new RegExp("90327 حسین کاظمی", "g"), "دکتر");
+
+    
+
     var users = document.getElementsByClassName("row user-row");
     for (var i = 0; i < users.length; i++)
     {
