df3fd3031d1a290d94b45dc09987922809261f63
Seyed Danial Movahed <movaheddanial@gmail.com>
2022-03-09 13:19:08 +0330

    Added "asarzade" nickname

--- skyroomCustom/script.js
+++ skyroomCustom/script.js
@@ -98,6 +98,8 @@ setInterval(function(){
     replace(document.getElementsByClassName("col box block users")[0], new RegExp("90308 محمدمتین براتی", "g"), "افزایش‌گر سطح علمی");
     replace(document.getElementsByClassName("col box-grow message-box")[0], new RegExp("90327 حسین کاظمی", "g"), "دکتر");
     replace(document.getElementsByClassName("col box block users")[0], new RegExp("90327 حسین کاظمی", "g"), "دکتر");
+    replace(document.getElementsByClassName("col box-grow message-box")[0], new RegExp("90323 کسری عصارزاده", "g"), "پدر میکی موس");
+    replace(document.getElementsByClassName("col box block users")[0], new RegExp("90323 کسری عصارزاده", "g"), "پدر میکی موس");
 
     
 
